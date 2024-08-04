0538223b75d2a74f1bc3009a678c629ed1a11110
NanuCR7 <harsha.sriharsha3@gmail.com>
2024-08-04 16:40:04 +0530

    v2 Spring cloud Config

---
+++ section6/v2-spring-cloud-config/accounts/src/test/java/com/easzybites/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easzybites.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = AccountsApplicationTests.class)
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
