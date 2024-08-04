3eea1182dc7d8adb5b71aea794ec9cef947993e3
NanuCR7 <harsha.sriharsha3@gmail.com>
2024-08-04 17:02:31 +0530

    Modified for application.yml

--- section6/v2-spring-cloud-config/accounts/src/test/java/com/eazybytes/accounts/AccountsApplicationTests.java
+++ section6/v2-spring-cloud-config/accounts/src/test/java/com/eazybytes/accounts/AccountsApplicationTests.java
@@ -3,7 +3,7 @@ package com.eazybytes.accounts;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AccountsApplicationTests.class)
 class AccountsApplicationTests {
 
 	@Test
