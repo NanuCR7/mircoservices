e53d9c9c5340deee2e0019042f2215751290ca5c
NanuCR7 <harsha.sriharsha3@gmail.com>
2024-10-04 14:43:23 +0530

    Added new section for Gatewat server

---
+++ section9/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,39 @@
+package com.eazybytes.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.eazybytes.accounts.dto.AccountsContactInfoDto;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@SpringBootApplication
+@EnableFeignClients
+/*
+ * @ComponentScans({ @ComponentScan("com.easzybites.accounts.controller") })
+ * 
+ * @EnableJpaRepositories("com.easzybites.accounts.repository")
+ * 
+ * @EntityScan("com.easzybites.accounts.model")
+ */
+@EnableConfigurationProperties(value = AccountsContactInfoDto.class)
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(info = @Info(title = "Accounts microservice REST API Documentation", description = "EazyBank Accounts microservice", version = "v1", contact = @Contact(name = "Sriharsha", email = "harsha@gmail.com", url = "https://www.eazybites.com"), license = @License(name = "Apache 2.0")), externalDocs = @ExternalDocumentation(description = "EazyBank Accounts microservice", url = "https://www.eazybites.com/swagger-ui.html"))
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
---
+++ section9/accounts/src/main/java/com/eazybytes/accounts/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,52 @@
+package com.eazybytes.accounts.service.impl;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.eazybytes.accounts.dto.AccountsDto;
+import com.eazybytes.accounts.dto.CardsDto;
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
+import com.eazybytes.accounts.dto.LoansDto;
+import com.eazybytes.accounts.entity.Accounts;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.exceptions.ResourceNotFoundException;
+import com.eazybytes.accounts.mapper.AccountsMapper;
+import com.eazybytes.accounts.mapper.CustomerMapper;
+import com.eazybytes.accounts.repository.AccountsRepository;
+import com.eazybytes.accounts.repository.CustomerRepository;
+import com.eazybytes.accounts.service.ICustomerService;
+import com.eazybytes.accounts.service.client.CardsFeignClient;
+import com.eazybytes.accounts.service.client.LoansFeignClient;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class CustomerServiceImpl implements ICustomerService {
+
+	private AccountsRepository accountsRepository;
+	private CustomerRepository customerRepository;
+	private CardsFeignClient cardsFeignClient;
+	private LoansFeignClient loansFeignClient;
+
+	@Override
+	public CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationID) {
+		Customer cust = customerRepository.findByMobileNumber(mobileNumber)
+				.orElseThrow(() -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber));
+
+		Accounts acc = accountsRepository.findByCustomerId(cust.getCustomerId()).orElseThrow(
+				() -> new ResourceNotFoundException("Account", "customerId", cust.getCustomerId().toString()));
+
+		CustomerDetailsDto custDto = CustomerMapper.mapToCustomerDetailsDto(cust, new CustomerDetailsDto());
+		custDto.setAccountsDto(AccountsMapper.mapToAccountDto(acc, new AccountsDto()));
+
+		ResponseEntity<LoansDto> loansDtoRes = loansFeignClient.fetchLoanDetails(correlationID, mobileNumber);
+		custDto.setLoansDto(loansDtoRes.getBody());
+
+		ResponseEntity<CardsDto> cardsDtoRes = cardsFeignClient.fetchCardDetails(correlationID, mobileNumber);
+		custDto.setCardsDto(cardsDtoRes.getBody());
+
+		return custDto;
+	}
+
+}
